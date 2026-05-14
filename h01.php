171ac291396612015336804093c351fbdd38ffd6
apodolenko-a11y <apodolenko@hkhk.edu.ee>
2026-05-14 09:19:13 +0300

    hpo

--- h01.php
+++ h01.php
@@ -1,6 +1,23 @@
 <?php
+// Harjutus 01
+// Anastasiia
+// Kuupäev: 13.05.2026
 
-echo "Tere maailm";
+//Programm väljastab kolme muutuja (nimi, sünniaasta, tähtkuju) väärtused ühe koodireaga. Kusjuures kõik on väljastades eraldi real. Vastus täislausega.
+$nimi = "Mari";
+$sunniaasta = 2008;
+$tahtkuju = "Sõnn";
 
+echo "Minu nimi on " . $nimi . ".\nMinu sünniaasta on " . $sunniaasta . ".\nMinu tähtkuju on " . $tahtkuju . ".\n";
 
+echo "\n"; // Väike vahe sisse
+
+//Väljasta järgnev lause: “It’s My Life” – Dr. Alban
+echo "\"It's My Life\" - Dr. Alban\n\n";
+
+//Pilt
+echo " (\\(\\ \n";
+echo " ( -.-)\n";
+echo " o_(\")(\")\n";
 ?>
+
